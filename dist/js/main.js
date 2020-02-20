2d04f11738f284b8b4ea8819fd5940addb4059ea
Alexander <kurbatov199@gmail.com>
2020-02-20 19:36:50 +0300

    Собрана анимация карты в углу

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
 
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider__arrows slider__arrows--left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider__arrows slider__arrows--right" src="img/arrows-right.svg" alt="">',
+         asNavFor: '.slider__dots'
+    });
 
+    $('.slider__dots').slick({
+         slidesToShow: 4,
+         slidesToScroll: 4,
+         asNavFor: '.header__slider'
+    });
 
-  
 });
